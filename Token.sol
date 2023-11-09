9723ce64065b4ff4648c04862d34c221fa0973ce
Akpan Joseph <65995144+majiicmajic@users.noreply.github.com>
2023-11-09 09:52:14 +0100

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -11,12 +11,12 @@ import "./TRC20Detailed.sol";
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `TRC20` functions.
  */
-contract Token is TRC20, TRC20Detailed {
+contract MajicToken is TRC20, TRC20Detailed {
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public TRC20Detailed("YourTokenName", "YTN", 18) {
+    constructor () public TRC20Detailed("MajicToken", "MJC", 18) {
         _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
     }
 }
